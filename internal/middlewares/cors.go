8c6dd29ad68434c524e18277884ba6a68dc34971
Ayush Vyas <work.ayushvyas17@gmail.com>
2026-01-05 00:25:22 +0530

    Refactor CORS middleware to dynamically set Access-Control-Allow-Origin based on request origin

--- internal/middlewares/cors.go
+++ internal/middlewares/cors.go
@@ -5,13 +5,15 @@ import "net/http"
 func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-		// Allow your frontend
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:6000")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := r.Header.Get("Origin")
+		if origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+		}
+
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		// Handle preflight request
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
